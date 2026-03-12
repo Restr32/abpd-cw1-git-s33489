11bb05fd9f37bfb390cba8599a44145281c87349
Restr32 <s33489@pjwstk.edu.pl>
2026-03-12 14:32:04 +0100

    Initial project

---
+++ tutorial/tutorial/Program.cs
@@ -0,0 +1,11 @@
+﻿class tutorial
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj wyrażenie liczby do mnożenia: ");
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        a = a * b;
+        Console.WriteLine("Wynik: "+a);
+    }
+}
