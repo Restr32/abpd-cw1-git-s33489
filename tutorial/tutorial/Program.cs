340e5d09b460d1457057e640e74423104d0dce52
Restr32 <s33489@pjwstk.edu.pl>
2026-03-18 23:41:06 +0100

    wyświetlanie na gorze widomosci

--- tutorial/tutorial/Program.cs
+++ tutorial/tutorial/Program.cs
@@ -1,11 +1,5 @@
 ﻿
-Console.WriteLine("Podaj wyrażenie liczby do mnożenia: ");
-
-float a = float.Parse(Console.ReadLine());
-float b = float.Parse(Console.ReadLine());
-a *= b;
-
-Console.WriteLine("Wynik: " + a);
+Console.WriteLine(":) Początek kodu");
 
 Console.WriteLine("avg test: "+CalculateAverage(new []{1, 2, 3, 4, 5, 6}));
 
